ba6fb8607d3ff87c54d5a6e880a0605ee4ca59cf
Rora Alem <rora.goitom@gmail.com>
2023-12-12 18:08:17 -0500

    Wrap the whole app with BrowserRouter component to use react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+
   </React.StrictMode>
 );
