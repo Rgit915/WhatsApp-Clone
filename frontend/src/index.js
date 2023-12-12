bb8a2c24e35bb8e8c6f07784b4984c80e85b9b7f
Rora Alem <rora.goitom@gmail.com>
2023-12-12 15:38:12 -0500

    Add ColorModeScript component to set initial color  mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript/>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-
   </React.StrictMode>
 );
