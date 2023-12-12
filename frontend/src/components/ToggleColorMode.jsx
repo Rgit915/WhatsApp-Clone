0279b94af85053748591be28653ff58e6e244bed
Rora Alem <rora.goitom@gmail.com>
2023-12-12 16:36:58 -0500

    Add Chakra UI's useColorMode hook to manage & toggle between light & dark modes

--- frontend/src/components/ToggleColorMode.jsx
+++ frontend/src/components/ToggleColorMode.jsx
@@ -1,9 +1,14 @@
 import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/react";
 
 const ToggleColorMode = () => {
+  //useColorMode hook from chakra-ui
+  const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button > toggle </Button>
+    <Button onClick={() =>toggleColorMode()}>
+      {colorMode}
+    </Button>
 
 
   );
