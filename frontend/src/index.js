f9c3a0829831db1ca66d066d576ef9a6a4fd2d74
Rora Alem <rora.goitom@gmail.com>
2023-12-12 14:51:27 -0500

    Import & render ColorModeScript component from chakra-ui

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript/>
       <App />
     </ChakraProvider>
 
