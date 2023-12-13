398d39208cd9c200671445ed8a451e5cfdf8de60
Rora Alem <rora.goitom@gmail.com>
2023-12-13 07:21:33 -0500

    Add VStack form with username & password inputs

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { FormControl, FormLabel, VStack, Input, Button, ButtonGroup, FormErrorMessage } from '@chakra-ui/react';
+import React from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  return <VStack as="form"  w={{ base: "90%", md: "500px" }} justify="center" m="auto" h="100vh">
+    <FormControl>
+      <FormLabel >Username</FormLabel>
+      <Input name="username" placeholder="Enter Username" autoComplete="off" />
+      <FormErrorMessage>Invalid Username</FormErrorMessage>
+    </FormControl>
 
-export default Login
+    <FormControl>
+      <FormLabel >Password</FormLabel>
+      <Input name="password" placeholder="Enter Password" type="password" autoComplete="off"/>
+      <FormErrorMessage>Invalid password</FormErrorMessage>
+    </FormControl>
+<ButtonGroup>
+   <Button  type="submit">Log in</Button>
+   <Button>Create Account</Button>
+</ButtonGroup>
+
+  </VStack>;
+
+};
+
+export default Login;
