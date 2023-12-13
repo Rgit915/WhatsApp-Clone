a457d7c6a320cd748d8520faa0f2f81e45ec405d
Rora Alem <rora.goitom@gmail.com>
2023-12-13 12:09:23 -0500

    Wrap with Formik component  & transfer the arguments as props

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,67 +4,68 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 const Login = () => {
 
-  const formik = useFormik({
-    initialValues: { username: "", password: "" }, //Formik uses the name of the Input tp track its value
-    validationSchema: Yup.object({
-      username: Yup.string()
-        .required("Username required!") //string that shows user error message when username not provided
-        .min(6, "username too short!")
-        .max(28, "Username too long"),
-      password: Yup.string()
-        .required("Password required!") //string that shows user error message when password not provided
-        .min(6, "Password too short!")
-        .max(28, "Password too long")
-    }),
-    // onSubmit function called when the form is submitted
-    onSubmit: (values, actions) => {
-      // Displaying form values as an alert
-      alert(JSON.stringify(values, null, 2));
-
-      // Resetting the form after submission
-      actions.resetForm();
-    }
-  });
+  const formik = useFormik({});
+  
   return (
-    <VStack
-      as="form" w={{ base: "90%", md: "500px" }}
-      justify="center"
-      m="auto"
-      h="100vh"
-      spacing="1rem"
-      onSubmit={formik.handleSubmit}
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required("Username required!")
+          .min(6, "username too short!")
+          .max(28, "Username too long"),
+        password: Yup.string()
+          .required("Password required!")
+          .min(6, "Password too short!")
+          .max(28, "Password too long")
+      })}
+
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
     >
-      <Heading>Log In</Heading>
-      <FormControl isInvalid={formik.errors.username && formik.touched}>
-        <FormLabel fontSize="lg" >Username</FormLabel>
-        <Input
-          name="username"
-          placeholder="Enter Username"
-          autoComplete="off"
-          size="lg"
-          {...formik.getFieldProps("username")}
-        />
-        <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
-      </FormControl>
+      <VStack
+        as="form" w={{ base: "90%", md: "500px" }}
+        justify="center"
+        m="auto"
+        h="100vh"
+        spacing="1rem"
+        onSubmit={formik.handleSubmit}
+      >
+        <Heading>Log In</Heading>
+        <FormControl isInvalid={formik.errors.username && formik.touched}>
+          <FormLabel fontSize="lg" >Username</FormLabel>
+          <Input
+            name="username"
+            placeholder="Enter Username"
+            autoComplete="off"
+            size="lg"
+            {...formik.getFieldProps("username")}
+          />
+          <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
+        </FormControl>
 
-      <FormControl isInvalid={formik.errors.password && formik.touched}>
-        <FormLabel fontSize="lg">Password</FormLabel>
-        <Input
-          name="password"
-          placeholder="Enter Password"
-          type="password"
-          autoComplete="off"
-          size="lg"
-          {...formik.getFieldProps("password")}
-        />
-        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
-      </FormControl>
-      <ButtonGroup pt="1rem">
-        <Button type="submit" colorScheme="teal">Log in</Button>
-        <Button>Create Account</Button>
-      </ButtonGroup>
+        <FormControl isInvalid={formik.errors.password && formik.touched}>
+          <FormLabel fontSize="lg">Password</FormLabel>
+          <Input
+            name="password"
+            placeholder="Enter Password"
+            type="password"
+            autoComplete="off"
+            size="lg"
+            {...formik.getFieldProps("password")}
+          />
+          <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+        </FormControl>
+        <ButtonGroup pt="1rem">
+          <Button type="submit" colorScheme="teal">Log in</Button>
+          <Button>Create Account</Button>
+        </ButtonGroup>
 
-    </VStack>
+      </VStack>
+    </Formik>
+
   );
 
 };
