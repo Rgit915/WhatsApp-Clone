1255daf9ca91afa54ca3095ac9b95659751a7eb3
Rora Alem <rora.goitom@gmail.com>
2023-12-18 17:35:02 -0500

    Implement form submission logic in front end

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -22,8 +22,44 @@ const Login = () => {
       })}
 
       onSubmit={(values, actions) => {
-        alert(JSON.stringify(values, null, 2));
+        // Create a shallow copy of the 'values' object
+        const vals = { ...values };
+
+        // Reset the form using the 'resetForm' method from the 'actions' object
         actions.resetForm();
+
+        // Perform a POST request
+        fetch("http://localhost:4000/auth/login", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Send the JSON-serialized copy of the 'values' object in the request body
+          body: JSON.stringify(vals),
+
+        })
+          .catch(err => {
+            // If an error occurs, simply return and do nothing
+            return;
+          })
+          .then(res => {
+
+            // Check if the response is not OK or has a status code of 400 or higher
+            if (!res || !res.ok || res.status >= 400) {
+              return;
+            }
+
+            // Parse the response body as JSON and return the result
+            return res.json();
+          })
+          // Handle the parsed JSON data
+          .then(data => {
+            // If there is no data, return and do nothing
+            if (!data) return;
+
+            console.log(data);
+          });
       }}
     >
       {/* Formik wrapper for managing form state */}
--- frontend/src/components/Login/SignUp.jsx
+++ frontend/src/components/Login/SignUp.jsx
@@ -4,7 +4,7 @@ import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import TextField from './TextField';
 import { useNavigate } from 'react-router-dom';
-import {ArrowBackIcon} from '@chakra-ui/icons'
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 const SignUp = () => {
 
@@ -25,8 +25,44 @@ const SignUp = () => {
       })}
 
       onSubmit={(values, actions) => {
-        alert(JSON.stringify(values, null, 2));
+        // Create a shallow copy of the 'values' object
+        const vals = { ...values };
+
+        // Reset the form using the 'resetForm' method from the 'actions' object
         actions.resetForm();
+
+        // Perform a POST request
+        fetch("http://localhost:4000/auth/register", {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // Send the JSON-serialized copy of the 'values' object in the request body
+          body: JSON.stringify(vals),
+
+        })
+          .catch(err => {
+            // If an error occurs, simply return and do nothing
+            return;
+          })
+          .then(res => {
+
+            // Check if the response is not OK or has a status code of 400 or higher
+            if (!res || !res.ok || res.status >= 400) {
+              return;
+            }
+
+            // Parse the response body as JSON and return the result
+            return res.json();
+          })
+          // Handle the parsed JSON data
+          .then(data => {
+            // If there is no data, return and do nothing
+            if (!data) return;
+
+            console.log(data);
+          });
       }}
     >
       {/* Formik wrapper for managing form state */}
@@ -62,7 +98,7 @@ const SignUp = () => {
           <Button type="submit" colorScheme="teal">
             Create Account
           </Button>
-          <Button onClick={() =>navigate("/")} leftIcon={<ArrowBackIcon/>} >Back</Button>
+          <Button onClick={() => navigate("/")} leftIcon={<ArrowBackIcon />} >Back</Button>
         </ButtonGroup>
       </VStack>
     </Formik>
