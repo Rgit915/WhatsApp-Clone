16da7159f27e49ad6d6c9f27551965e593643d6a
Rora Alem <rora.goitom@gmail.com>
2023-12-18 16:34:52 -0500

    Modify the setup of express server with socket.io integration

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,47 @@
 //Import express module
 const express = require('express');
+// Import Server class from socket.io for handling WebSocket connections
+const { Server } = require('socket.io');
+// Import helmet for setting various HTTP headers to enhance security
+const helmet = require('helmet');
 
-const port = 8080;
+const port = 4000;
 
 // Create an instance of the express app
 const app = express();
 
-// Define a route for handling HTTP GET requests to the root path '/'
-app.get('/' ,(request, response) =>{
+// Create a server that handles both HTTP and socket.io requests
+const server = require("http").createServer(app);
 
-  response.send("Express server!")
-})
+// Create a socket.io instance attached to the server, with CORS configuration
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+// Add middleware - Use helmet
+app.use(helmet());
+
+// Parse JSON requests
+app.use(express.json());
+
+// Define a route for handling HTTP GET requests to the root path '/'
+app.get('/', (request, response) => {
+
+  response.json("Express server!");
+});
+
+// Handle WebSocket connections when a client connects to the server
+io.on("connect", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+
+  // Send a welcome message to the connected client
+  socket.emit("message", "Welcome to the WebSocket server!");
+});
+
+
 
 // Start the express app and make it listen on the specified port
 app.listen(port, () => {
