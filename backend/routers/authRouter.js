6b5fb294099b0e8efd1684890d734b6f4a2b3659
Rora Alem <rora.goitom@gmail.com>
2023-12-19 16:39:31 -0500

    Add status response code

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -24,12 +24,16 @@ router.post("/login", (req, res) => {
     .catch(err => {
       // Log validation errors to the console
       console.log(err.errors);
+
+      // Handle validation errors (send an error response to the client)
+      res.status(422).json({ errors: err.errors });
     })
     .then(valid => {
       if (valid) {
         // The form is valid, handle the login logic here
         console.log("Form is valid!");
 
+        res.status(200).json({ message: "Login successful" });
       }
     });
 });
