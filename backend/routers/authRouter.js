9cbcad54e334dab78f6f551a0b8de2a28775207e
Rora Alem <rora.goitom@gmail.com>
2023-12-19 15:56:45 -0500

    Define a Yup schema for form validation

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Yup = require('yup');
+
+//Creates a Yup object schema that defines the shape of the object to be validated
+const formSchema = Yup.object({
+  username: Yup.string()
+    .required("Username required")
+    .min(6, "Username too short")
+    .max(28, "Username too long"),
+  password: Yup.string()
+    .required("Username required")
+    .min(6, "Username too short")
+    .max(28, "Username too long"),
+});
 
 module.exports = router;
