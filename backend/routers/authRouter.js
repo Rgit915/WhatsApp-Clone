1df109ff8fc142dcd2aa56cd21b96afc39edd73c
Rora Alem <rora.goitom@gmail.com>
2023-12-19 16:43:04 -0500

    Add signup route with form validation

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -38,5 +38,28 @@ router.post("/login", (req, res) => {
     });
 });
 
+// create a route for /signup
+router.post("/signup", (req, res) => {
+  const formData = req.body;
+
+  // Validate the form data using Yup schema
+  formSchema
+    .validate(formData)
+    .catch(err => {
+      // Log validation errors to the console
+      console.log(err.errors);
+
+      // Handle validation errors (send an error response to the client)
+      res.status(422).json({ errors: err.errors });
+    })
+    .then(valid => {
+      if (valid) {
+        // The form is valid, handle the login logic here
+        console.log("Form is valid!");
+
+        res.status(200).json({ message: "Login successful" });
+      }
+    });
+});
 
 module.exports = router;
