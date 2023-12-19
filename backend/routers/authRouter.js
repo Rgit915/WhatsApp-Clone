ae604c7225ca032fe4514530ca46095f3205ecdd
Rora Alem <rora.goitom@gmail.com>
2023-12-19 17:07:37 -0500

    Refactor code

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,65 +1,15 @@
 const express = require('express');
+const validateForm = require('../controllers/validateForm');
 const router = express.Router();
-const Yup = require('yup');
-
-//Creates a Yup object schema that defines the shape of the object to be validated
-const formSchema = Yup.object({
-  username: Yup.string()
-    .required("Username required")
-    .min(6, "Username too short")
-    .max(28, "Username too long"),
-  password: Yup.string()
-    .required("Username required")
-    .min(6, "Username too short")
-    .max(28, "Username too long"),
-});
 
 // create a route for /login
 router.post("/login", (req, res) => {
-  const formData = req.body;
-
-  // Validate the form data using Yup schema
-  formSchema
-    .validate(formData)
-    .catch(err => {
-      // Log validation errors to the console
-      console.log(err.errors);
-
-      // Handle validation errors (send an error response to the client)
-      res.status(422).json({ errors: err.errors });
-    })
-    .then(valid => {
-      if (valid) {
-        // The form is valid, handle the login logic here
-        console.log("Form is valid!");
-
-        res.status(200).json({ message: "Login successful" });
-      }
-    });
+ validateForm(req, res);
 });
 
 // create a route for /signup
 router.post("/signup", (req, res) => {
-  const formData = req.body;
-
-  // Validate the form data using Yup schema
-  formSchema
-    .validate(formData)
-    .catch(err => {
-      // Log validation errors to the console
-      console.log(err.errors);
-
-      // Handle validation errors (send an error response to the client)
-      res.status(422).json({ errors: err.errors });
-    })
-    .then(valid => {
-      if (valid) {
-        // The form is valid, handle the login logic here
-        console.log("Form is valid!");
-
-        res.status(200).json({ message: "Login successful" });
-      }
-    });
+  validateForm(req, res);
 });
 
 module.exports = router;
