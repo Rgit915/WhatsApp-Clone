47716837e0d6b8f11128635ba68e0f49cf17c308
Rora Alem <rora.goitom@gmail.com>
2023-12-22 12:55:02 -0500

    Improve user registration logic to efficiently check for existing usernames

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const validateForm = require('../controllers/validateForm');
 const router = express.Router();
+const pool = require('../db');
+const bcrypt = require('bcrypt');
 
 // create a route for /login
 router.post("/login", (req, res) => {
- validateForm(req, res);
+  validateForm(req, res);
 });
 
 // create a route for /signup
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   validateForm(req, res);
+
+  // Check if the username already exists in the database
+  const existingUser = await pool.query(
+    "SELECT username FROM users WHERE username = $1",
+    [req.body.username]
+  );
+
+  if (existingUser.rowCount === 0) {
+   // If the username doesn't exist, register the new user
+    const hashedPass = await bcrypt.hash(req.body.password, 10);
+    const newUserQuery = await pool.query(
+      "INSERT INTO users(username, passhash) values($1,$2) RETURNING username",
+      [req.body.username, hashedPass]
+    );
+    res.json({ loggedIn: true, username });
+  } else {
+     // If the username already exists, send a response indicating it's taken
+    res.json({ loggedIn: false, status: "Username taken" });
+
+  }
 });
 
 module.exports = router;
